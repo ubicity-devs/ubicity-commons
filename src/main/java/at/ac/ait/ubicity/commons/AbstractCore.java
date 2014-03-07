b02a5ebd7d03572cc4c60ff03e6c45277bc930c7
Jan van Oort <jan.vanoort@ait.ac.at>
2014-03-07 22:56:03 +0100

    Began preparations for add-ons in commons

--- src/main/java/at/ac/ait/ubicity/commons/AbstractCore.java
+++ src/main/java/at/ac/ait/ubicity/commons/AbstractCore.java
@@ -1,6 +1,24 @@
+/**
+    Copyright (C) 2013  AIT / Austrian Institute of Technology
+    http://www.ait.ac.at
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/agpl-3.0.html
+ */
 
 package at.ac.ait.ubicity.commons;
 
+import at.ac.ait.ubicity.commons.interfaces.UbicityAddOn;
 import at.ac.ait.ubicity.commons.interfaces.UbicityPlugin;
 import at.ac.ait.ubicity.commons.protocol.Answer;
 import at.ac.ait.ubicity.commons.protocol.Command;
@@ -37,7 +55,8 @@ public  class AbstractCore implements Runnable {
     
     //here we keep track of currently running plugins
     protected final Map< UbicityPlugin, PluginContext > plugins = new HashMap();
-        
+       
+    protected final Map< UbicityAddOn, AddOnContext > addOns = new HashMap();
     
     //our elasticsearch indexing client
     protected static final TransportClient esclient;
---
+++ src/main/java/at/ac/ait/ubicity/commons/interfaces/UbicityAddOn.java
@@ -0,0 +1,44 @@
+
+/**
+    Copyright (C) 2013  AIT / Austrian Institute of Technology
+    http://www.ait.ac.at
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/agpl-3.0.html
+ */
+
+package at.ac.ait.ubicity.commons.interfaces;
+
+import at.ac.ait.ubicity.commons.JSONObjectHandler;
+import com.lmax.disruptor.EventHandler;
+import java.util.Set;
+import java.util.SortedSet;
+import net.xeoh.plugins.base.Plugin;
+
+/**
+ *
+ *  "Marker" interface for all ubicity add-ons. Add-ons are basically different from 
+ * plugins in that they 
+ * 1) may bring EventHandlers for dealing with JSONObjects
+ * 2) provide a set of functionalities different from ( "stupid" or ReverseControllable ) plugins
+ * 
+ * @author jan
+ */
+public interface UbicityAddOn extends Plugin {
+ 
+    
+    
+    public SortedSet< Class< ? extends JSONObjectHandler >  >  getDeclaredEventHandlers();
+    
+    public boolean shutDown();
+}
